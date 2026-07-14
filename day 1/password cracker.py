858742812b4945087dca17f2c0e436e4597b5a67
lorenzoxkurtz <lorenzokurtz8@gmail.com>
2026-07-14 18:11:25 -0300

    day1 pt2

--- day 1/password cracker.py
+++ day 1/password cracker.py
@@ -1,4 +1,5 @@
 n=50
+newpassword=0
 p=0
 with open ('s.txt') as file:
    s= file.read().splitlines()
@@ -7,12 +8,20 @@ for r in s :
   ds=int(r[1:])
   if dr=='L':
     ds=-ds
+    xtra=-1
+  else:
+     xtra=1
+  oldn=n
   n=n+ds
-  n=n%100
+  for i in range(oldn*xtra,n*xtra):
+     if i%100==0:
+       newpassword+=1    
+  n= n%100
   if n==0:
      p=p+1
 
 print (p)
+print(newpassword)
 
 
 
