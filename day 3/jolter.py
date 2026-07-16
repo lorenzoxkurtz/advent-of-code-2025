aa62802a3bf8c6a497ef5647069204c6941e5d76
lorenzoxkurtz <lorenzokurtz8@gmail.com>
2026-07-16 18:21:25 -0300

    day 3 pt1

---
+++ day 3/jolter.py
@@ -0,0 +1,17 @@
+joltage=0
+with open ('banks') as file:
+    banks= file.read().splitlines()
+    for batteries in banks:
+        biggest1=-1
+        biggest2=-1
+        biggest1position=-1 
+        for position, battery in enumerate(batteries):
+            if int(battery) > biggest1 and position != len(batteries)-12:
+                biggest1 = int(battery)
+                biggest1position=position       
+        for position, battery in enumerate(batteries):
+            if int(battery)>biggest2 and position> biggest1position:
+                biggest2=int(battery)
+        joltage+=(biggest1*10) + biggest2
+print(joltage)
+    
